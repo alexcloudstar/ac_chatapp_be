44cb7fbc5d7749a28cea324b671f2b7ce0c26426
alexcloudstar <alexcloudstar@gmail.com>
2022-11-24 13:30:03 +0200

    feat: UI Tweaks

--- client/src/pages/chat/components/Header/Header.tsx
+++ client/src/pages/chat/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { FaTrashAlt } from 'react-icons/fa'
+import { FaEdit, FaTrashAlt } from 'react-icons/fa'
 import { useParams } from 'react-router-dom'
 
 import { ConversationType } from 'components/ChatList/types'
@@ -19,12 +19,23 @@ const Header = () => {
     console.log('delete room')
   }
 
+  const onChangeName = () => {}
+
   return (
     <div className="conversation-header flex items-center justify-center">
       <h1 className="mr-2">{conversation?.name}</h1>
       {conversation?.userOwnerId === user?.id && (
-        <div className="actions">
-          <Icon icon={<FaTrashAlt />} onClick={onDeleteRoom} />
+        <div className="actions flex">
+          <Icon
+            icon={<FaTrashAlt />}
+            onClick={onDeleteRoom}
+            classes="mx-4 text-[22px]"
+          />
+          <Icon
+            icon={<FaEdit />}
+            onClick={onChangeName}
+            classes="text-[22px]"
+          />
         </div>
       )}
     </div>
